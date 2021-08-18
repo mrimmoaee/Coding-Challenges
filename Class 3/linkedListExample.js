d3b549fa31c7537c1e76f8732817d7bccb36fae6
mrimmoaee <mrimmoaee@gmail.com>
2021-08-18 07:50:16 +0600

    linked list example in js

---
+++ Class 3/linkedListExample.js
@@ -0,0 +1,114 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+// const n1 = new Node(100);
+// console.log(n1);
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Insert first node
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+    this.size++;
+  }
+
+  // Insert last node
+  insertLast(data) {
+    let node = new Node(data);
+    let current;
+
+    // if empty, make head
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+
+    this.size++;
+  }
+
+  // Insert at index
+  insertAt(data, index) {
+    // if index is out of range
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    // If first index
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    // Set current to first
+    current = this.head;
+    let count = 0;
+
+    while (count < index) {
+      previous = current; //Node before index
+      count++;
+      current = current.next; //Node after index
+    }
+
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
+  }
+
+  // Get at index
+  getAt(index) {
+    let current = this.head;
+    let count = 0;
+
+    while (current) {
+      if (count == index) {
+        console.log(current.data);
+      }
+
+      count++;
+      current = current.next;
+    }
+
+    return null;
+  }
+
+  // Remove at index
+  // Clear list
+  // Print List Data
+  printListData() {
+    let current = this.head;
+
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const ll = new LinkedList();
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(400);
+
+ll.printListData();
+ll.getAt(2);
+
+console.log(ll);
