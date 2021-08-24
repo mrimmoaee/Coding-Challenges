78f52563549fb0a6848c4b4125315b432bba571c
mrimmoaee <mrimmoaee@gmail.com>
2021-08-24 11:33:39 +0600

    completed problem 122

---
+++ Class 2/Problem 122/buyandsell2.js
@@ -0,0 +1,12 @@
+var maxProfit = function (prices) {
+  let maxprofit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    // Add to the max profit total if the current price is greater than previous
+    // We "sell" when the the opposite is true because we will not buy on days that dip.
+    if (prices[i] > prices[i - 1])
+      // Add the increse in price to our profit
+      maxprofit += prices[i] - prices[i - 1];
+    console.log(maxprofit);
+  }
+  return maxprofit;
+};
